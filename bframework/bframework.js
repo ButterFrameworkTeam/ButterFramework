c717b23e4195d2d1beb9f9eb5ec3ed99c0fc054b
Buttershy <buttershy@laposte.net>
2014-07-23 14:44:23 +0200

    Medium Update
    - Moving BFramework.less
    - New table style
    - code.less, colors.less and button.less new code stucture
    - Removed Custom Font
    - Adding Utils.less with .border-radius() and .float()
    - Code.less padding Adjusted
    - Adding bfdouble(), bfGiveRound() in bframework.js and changing bfcarre
    to bfSquare

--- bframework/bframework.js
+++ bframework/bframework.js
@@ -12,21 +12,36 @@ function bfCube(Cube)
 {
     return Cube*Cube*Cube; // Return the cube
 }
-function bfCarre(Square) // Return Square
+
+function bfSquare(Square) // Return Square
 {
     return Square*Square;
 }
+
+function bfdouble(Double) 
+{
+    return Double*2; // Return the double
+}
+
 function bftriple(Triple)
 {
     return Triple*3; // Return the Triple
 }
-function bfchoosenumber(min, max) 
+
+function bfchoosenumber(Min, max) 
 {
-     var a = min + (max-min+1)*Math.random();
-     return Math.floor(a); // return a number between min and max
+     var bfchoosenum = min + (max-min+1)*Math.random();
+     return Math.floor(bfchoosenum); // return a number between min and max
 }
+
 function bfrndnumber(NotHighterThan) // Return a number between 0 and the var NotHighterThan
 { 
     var rndnumber = Math.floor(Math.random()*NotHighterThan)+1;
     return rndnumber; // Return 
+}
+
+function bfGiveRound(Round) // Give the Round
+{
+    var bfGiveRoundvar = Math.round(Round);
+    return bfGiveRoundvar;
 }
