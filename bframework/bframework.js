7eaea2c5e2f2f84cf5520c8a58f66d99592055cf
Buttershy <buttershy@laposte.net>
2014-08-03 00:06:25 +0200

    The most biggest UPDATE ! Check desc.
    - Changed Less to Sass
    - Adding grid system
    - converted Less function to Sass funtions
    - Corrected button, now use <button class="button"> for the Framework
    Custom Button.
    - Fixed stuff
    - Updated defaultpage.html
    - BFramework JS have the custom Grid system.
    - Removed some files

--- bframework/bframework.js
+++ bframework/bframework.js
@@ -45,6 +45,87 @@ function bfGiveRound(Round) // Give the Round
     var bfGiveRoundvar = Math.round(Round);
     return bfGiveRoundvar;
 }
+
+// ======== Grid ========
+
+;(function() {
+
+  "use strict";
+
+  var options = {
+    'maxPageWidth' :     '1000px',                      // set overlay width to match page width (as max-width for flexible grids)
+    'columnWidth' :      '150px',                       // set column width in px, em or %
+    'columnCount' :       6,                            // set column count
+    'columnColor' :      'rgba(255, 0, 0, 0.25)',       // set column color
+    'gutterWidth' :      '20px',                        // set gutter width in px, em or %
+    'baselineHeight' :    22,                           // set baseline height (requires pixels for now)
+    'baselineColor' :    'rgba(255, 255, 255, 1)',      // set baseline color
+    'initialDisplay' :   'none'                         // use 'block' to have grid visible by default
+  },
+
+  // parse integers from option variables
+  maxPageWidthInt = parseInt(options.maxPageWidth, 10),
+  columnWidthInt = parseInt(options.columnWidth, 10),
+  gutterWidthInt = parseInt(options.gutterWidth, 10),
+
+  // calculate column and gutter widths in percentages
+  columnPercentage = ( columnWidthInt / maxPageWidthInt ) * 100 + '%',
+  gutterPercentage = ( gutterWidthInt / maxPageWidthInt ) * 100 + '%',
+
+  // create baseline grid gradient
+  baselineGradient = ( options.baselineHeight - 1 ) + 'px, ' + options.baselineColor + ' ' + options.baselineHeight + 'px',
+
+  // create gridOverlay element
+  gridOverlay = document.createElement('div');
+
+  // assign id to gridOverlay element
+  gridOverlay.id = 'gridOverlay';
+
+  // style gridOverlay element (max-width is overwritten manually for tutorial to compensate for body padding. padding and border box added for same reason)
+  gridOverlay.style.cssText = '\
+    display: ' + options.initialDisplay + '; \
+    position: absolute; top: 0; right: 0; bottom: 0; left: 0; margin: 0 auto; width: 100%; padding: 0 1em; box-sizing: border-box; \
+    max-width: 1032px; \
+    background-size: 100% ' + options.baselineHeight + 'px; \
+    background-image: -webkit-linear-gradient(top, transparent ' + baselineGradient + '); \
+    background-image:    -moz-linear-gradient(top, transparent ' + baselineGradient + '); \
+    background-image:     -ms-linear-gradient(top, transparent ' + baselineGradient + '); \
+    background-image:      -o-linear-gradient(top, transparent ' + baselineGradient + '); \
+    background-image:                 linear, top, transparent ' + baselineGradient + ');';
+
+  // add grid container to page
+  document.body.appendChild(gridOverlay);
+
+  // add columns to page and style them
+  for (var i = 0; i < options.columnCount; i++) {
+    var column = document.createElement('div');
+    gridOverlay.appendChild(column);
+    column.style.cssText = '\
+      display: block; float: left; position: relative; height: auto; min-height: 100%; \
+      background-color: ' + options.columnColor + '; \
+      margin-right: ' + gutterPercentage + '; \
+      width: ' + columnPercentage + ';';
+  };
+
+  // remove right margin from last column
+  gridOverlay.lastChild.style.marginRight = 0;
+
+  // style body to prevent scrolling grid and match full page height
+  document.body.style.cssText = 'position: relative; height: auto; min-height: 100%';
+
+  // enable toggling of grid visibility with Option + G (Mac) or Alt + G (PC)
+  document.addEventListener('keyup', function (e) {
+    if (e.keyCode === 71 && e.altKey) {
+      if (gridOverlay.style.display === 'none') {
+        gridOverlay.style.display = 'block';
+      } else {
+        gridOverlay.style.display = 'none';
+      }
+    }
+  });
+
+})();
+
 // -----------------------------
 // Jquery 
 
